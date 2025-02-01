eea532d13996b3f87ffbb446c1e0f969c3f7a958
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2025-02-01 15:22:03 +0300

    add basic run

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vigo/tablo
+
+go 1.23.5
---
+++ internal/tablo/tablo.go
@@ -0,0 +1,12 @@
+/*
+Package tablo implements ...
+*/
+package tablo
+
+import "fmt"
+
+// Run ...
+func Run() error {
+	fmt.Println("running...")
+	return nil
+}
